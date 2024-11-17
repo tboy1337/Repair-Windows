64550fa9df69b4e2cec947759eb05d2c57e23440
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-11-17 12:06:41 +0000

    Update chkdsk_Scan_NTFS.cmd

--- chkdsk_Scan_NTFS.cmd
+++ chkdsk_Scan_NTFS.cmd
@@ -9,25 +9,25 @@ if %errorlevel% neq 0 (
     exit /b 2
 )
 
-cd /d "%SystemDrive%" >nul 2>&1
+cd /d "C:" >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to change to %SystemDrive% drive.
+    echo Failed to change to C: drive.
 )
 
-echo Checking %SystemDrive% file system...
-call chkdsk "%SystemDrive%" >nul 2>&1
+echo Checking C: file system...
+call chkdsk "C:" >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Issues found with %SystemDrive% file system.
-    echo y | call chkdsk "%SystemDrive%" /F /X /B /scan /perf >nul 2>&1
+    echo Issues found with C: file system.
+    echo y | call chkdsk "C:" /F /X /B /scan /perf >nul 2>&1
     echo Restarting system to complete repairs.
-    echo Run chkdsk "%SystemDrive%" /sdcleanup after repair finishes.
+    echo Run chkdsk "C:" /sdcleanup after repair finishes.
     timeout /t 30 /nobreak
     call shutdown /r /f /t 0 >nul 2>&1
     exit /b 1
 )
 
-echo No issues found with %SystemDrive% file system.
-echo Run chkdsk "%SystemDrive%" /sdcleanup next.
+echo No issues found with C: file system.
+echo Run chkdsk "C:" /sdcleanup next.
 
 timeout /t 15 /nobreak
 
