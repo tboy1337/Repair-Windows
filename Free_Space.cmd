de5e270bd53b7f1768b0320f541a28b41bf1dce2
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-11-17 12:09:20 +0000

    Update Free_Space.cmd

--- Free_Space.cmd
+++ Free_Space.cmd
@@ -9,20 +9,20 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-cd /d "%SystemDrive%" >nul 2>&1
+cd /d "C:" >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to change to %SystemDrive% drive.
+    echo Failed to change to C: drive.
 )
 
-echo Freeing up space on %SystemDrive% drive...
-call cleanmgr /d "%SystemDrive%" /verylowdisk >nul 2>&1
+echo Freeing up space on C: drive...
+call cleanmgr /d "C:" /verylowdisk >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to free up space on %SystemDrive% drive.
+    echo Failed to free up space on C: drive.
 )
 
-call cleanmgr /d "%SystemDrive%" /autoclean >nul 2>&1
+call cleanmgr /d "C:" /autoclean >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to delete old Windows update files on %SystemDrive% drive.
+    echo Failed to delete old Windows update files on C: drive.
 )
 
 timeout /t 5 /nobreak
