ce29d28e61086febcf58466b509dc22014926f82
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-11-23 16:35:54 +0000

    Update Free_Space.cmd

--- Free_Space.cmd
+++ Free_Space.cmd
@@ -1,6 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set "SET_PATH=C:\Windows\System32"
+set "OS_DRIVE=C:"
+
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo This script requires administrator privileges.
@@ -9,18 +12,18 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-cd /d "C:" >nul 2>&1
+cd /d "%SET_PATH%" >nul 2>&1
 if %errorlevel% neq 0 (
     echo Failed to change to C: drive.
 )
 
 echo Freeing up space on C: drive...
-call cleanmgr /d "C:" /verylowdisk >nul 2>&1
+call cleanmgr /d "%OS_DRIVE%" /verylowdisk >nul 2>&1
 if %errorlevel% neq 0 (
     echo Failed to free up space on C: drive.
 )
 
-call cleanmgr /d "C:" /autoclean >nul 2>&1
+call cleanmgr /d "%OS_DRIVE%" /autoclean >nul 2>&1
 if %errorlevel% neq 0 (
     echo Failed to delete old Windows update files on C: drive.
 )
