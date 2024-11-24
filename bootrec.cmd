87d45978b433504a2588f11bb1146e6e5cc1b2d7
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-11-24 21:59:22 +0000

    Update bootrec.cmd

--- bootrec.cmd
+++ bootrec.cmd
@@ -1,53 +1,47 @@
 @echo off
 setlocal enabledelayedexpansion
 
-net session >nul 2>&1
+cd /d "C:\Windows\System32\Boot" >nul 2>&1
 if %errorlevel% neq 0 (
-    echo This script requires administrator privileges.
-    echo Please right-click and select "Run as administrator".
-    timeout /t 5 /nobreak
-    exit /b 1
+    echo Failed to change to C: drive.
 )
 
-cd /d "%SystemDrive%" >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Failed to change to %SystemDrive% drive.
+for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo C:^|find "File System Name"') do (
+    echo %%A | findstr /i /r "^FAT" >nul
+    if not errorlevel 1 (
+        echo The C: drive is FAT-based, repairing the MBR...
+        call bootrec /fixmbr >nul 2>&1
+    )
 )
 
-echo Fixing MBR...
-call bootrec /fixmbr >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Failed to fix MBR.
-)
+:: cd /d :\\EFI\\Microsoft\\Boot\\ after assigning drive letter to system? or reserve? partition (ONLY FOR SYSTEM I THINK)
 
-echo Fixing BOOT...
+echo Writing a new boot sector on the system partition...
 call bootrec /fixboot >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to fix BOOT.
+    echo Failed to write a new boot sector on the system partition.
 )
 
-echo Rebuilding BCD...
-call bootrec /rebuildbcd >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Failed to rebuild BCD.
-    echo Trying again.
-    call bcdedit /export "%SystemDrive%\BCDBackup" >nul 2>&1
-    call cd /d "%SystemDrive%" >nul 2>&1
-    call cd boot
-    call attrib bcd -s -h -r >nul 2>&1
-    call ren "%SystemDrive%\boot\bcd" "bcd.old" >nul 2>&1
-    call bootrec /rebuildbcd >nul 2>&1
-)
-
-echo Scanning OS...
+echo Scanning all disks for Windows installations...
 call bootrec /scanos >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to Scan OS.
-    echo Trying again.
-    call ren "%SystemDrive%\bootmgr" "bootmgrbackup" >nul 2>&1
-    call bootrec /RebuildBcd >nul 2>&1
-    call bootrec /FixBoot >nul 2>&1
+    echo Failed to scan all disks for Windows installations, trying again...
+    call ren "C:\bootmgr" "bootmgrbackup" >nul 2>&1
+    call bootrec /rebuildbcd >nul 2>&1
     call bootrec /scanos >nul 2>&1
+    call bootrec /fixboot >nul 2>&1
+)
+
+echo Rebuilding the BCD store...
+call bootrec /rebuildbcd >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Failed to rebuild the BCD store, trying again...
+    call bcdedit /export "C:\BCDBackup" >nul 2>&1
+    call attrib bcd -s -h -r >nul 2>&1
+    call ren "c:\boot\bcd" "bcd.old" >nul 2>&1
+    call bootrec /rebuildbcd >nul 2>&1
+    call bootrec /scanos >nul 2>&1
+    call bootrec /fixboot >nul 2>&1
 )
 
 timeout /t 5 /nobreak
