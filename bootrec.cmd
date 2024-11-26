b3606e3b42daf13c1a55b1bc11e52534c7479b62
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-11-26 17:26:36 +0000

    Update bootrec.cmd

--- bootrec.cmd
+++ bootrec.cmd
@@ -40,8 +40,8 @@ call bootrec /scanos >nul 2>&1
 if %errorlevel% neq 0 (
     echo Failed to scan all disks for Windows installations, trying again...
     call ren "%WINDOWS_DRIVE%\bootmgr" "bootmgrbackup" >nul 2>&1
-    call bootrec /rebuildbcd >nul 2>&1
     call bootrec /scanos >nul 2>&1
+    call bootrec /rebuildbcd >nul 2>&1
     call bootrec /fixboot >nul 2>&1
 )
 
@@ -52,8 +52,8 @@ if %errorlevel% neq 0 (
     call bcdedit /export "%WINDOWS_DRIVE%\BCDBackup" >nul 2>&1
     call attrib bcd -s -h -r >nul 2>&1
     call ren "%WINDOWS_DRIVE%\boot\bcd" "bcd.old" >nul 2>&1
-    call bootrec /rebuildbcd >nul 2>&1
     call bootrec /scanos >nul 2>&1
+    call bootrec /rebuildbcd >nul 2>&1
     call bootrec /fixboot >nul 2>&1
 )
 
