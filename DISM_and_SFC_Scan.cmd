9add0b16ba2c36bf2081a6fce9dfc8aa42ff52e6
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-12-03 19:45:19 +0000

    Update DISM_and_SFC_Scan.cmd

--- DISM_and_SFC_Scan.cmd
+++ DISM_and_SFC_Scan.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "OS_DRIVE=C:"
-set "SET_PATH=%OS_DRIVE%\Windows\System32"
+set "CHOSEN_DRIVE=C:"
+set "SET_PATH=%CHOSEN_DRIVE%\Windows\System32"
 set "SFC_SUCCESS=0"
 
 net session >nul 2>&1
