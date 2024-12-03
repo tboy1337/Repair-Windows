4af72802932e2c259fa632c7a3fea0e0c033134f
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-12-03 19:47:23 +0000

    Update chkdsk_Scan.cmd

--- chkdsk_Scan.cmd
+++ chkdsk_Scan.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "OS_DRIVE=C:"
-set "SET_PATH=%OS_DRIVE%\Windows\System32"
+set "CHOSEN_DRIVE=C:"
+set "SET_PATH=%CHOSEN_DRIVE%\Windows\System32"
 
 net session >nul 2>&1
 if %errorlevel% neq 0 (
@@ -17,29 +17,29 @@ if %errorlevel% neq 0 (
     echo Failed to change to %SET_PATH%
 )
 
-for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %OS_DRIVE%^|find "File System Name"') do (
+for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %CHOSEN_DRIVE%^|find "File System Name"') do (
     echo %%A | findstr /i /r "^FAT" >nul
     if not errorlevel 1 (
-        echo %OS_DRIVE% drive is FAT-based.
-        echo Checking %OS_DRIVE% file system...
-        chkdsk "%OS_DRIVE%" >nul 2>&1
+        echo %CHOSEN_DRIVE% drive is FAT-based.
+        echo Checking %CHOSEN_DRIVE% file system...
+        chkdsk "%CHOSEN_DRIVE%" >nul 2>&1
         if !errorlevel! neq 0 (
-            echo Repairing %OS_DRIVE% file system...
-            echo y | chkdsk "%OS_DRIVE%" /R /X >nul 2>&1
+            echo Repairing %CHOSEN_DRIVE% file system...
+            echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
             echo Restarting system to complete repairs.
             timeout /t 30 /nobreak
             shutdown /r /f /t 1 >nul 2>&1
             exit /b 1
         )
     ) else (
-        echo %OS_DRIVE% drive is not FAT-based.
-        echo Checking %OS_DRIVE% file system...
-        chkdsk "%OS_DRIVE%" >nul 2>&1
+        echo %CHOSEN_DRIVE% drive is not FAT-based.
+        echo Checking %CHOSEN_DRIVE% file system...
+        chkdsk "%CHOSEN_DRIVE%" >nul 2>&1
         if !errorlevel! neq 0 (
-            echo Repairing %OS_DRIVE% file system...
-            echo y | chkdsk "%OS_DRIVE%" /R /X /scan /perf >nul 2>&1
+            echo Repairing %CHOSEN_DRIVE% file system...
+            echo y | chkdsk "%CHOSEN_DRIVE%" /R /X /scan /perf >nul 2>&1
             echo Restarting system to complete repairs.
-            echo Run chkdsk "%OS_DRIVE%" /sdcleanup after repair finishes.
+            echo Run chkdsk "%CHOSEN_DRIVE%" /sdcleanup after repair finishes.
             timeout /t 30 /nobreak
             shutdown /r /f /t 1 >nul 2>&1
             exit /b 1
@@ -47,7 +47,7 @@ for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %OS_DRIVE%^|find "
     )
 )
 
-echo Found no issues with %OS_DRIVE% file system.
+echo Found no issues with %CHOSEN_DRIVE% file system.
 
 timeout /t 5 /nobreak
 
