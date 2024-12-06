152e44ca86ec0d2ad5275d10cd822fa0603a463d
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-12-06 14:55:12 +0000

    Update chkdsk_Scan.cmd

--- chkdsk_Scan.cmd
+++ chkdsk_Scan.cmd
@@ -21,29 +21,21 @@ for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %CHOSEN_DRIVE%^|fi
     echo %%A | findstr /i /r "^FAT" >nul
     if not errorlevel 1 (
         echo %CHOSEN_DRIVE% drive is FAT-based.
-        echo Checking %CHOSEN_DRIVE% file system...
-        chkdsk "%CHOSEN_DRIVE%" >nul 2>&1
-        if !errorlevel! neq 0 (
-            echo Repairing %CHOSEN_DRIVE% file system...
-            echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
-            echo Restarting system to complete repairs, please save your work.
-            timeout /t 30 /nobreak
-            shutdown /r /t 1 >nul 2>&1
-            exit /b 1
-        )
+        echo Repairing %CHOSEN_DRIVE% file system...
+        echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+        echo Restarting system to complete repairs, please save your work.
+        timeout /t 30 /nobreak
+        shutdown /r /t 1 >nul 2>&1
+        exit /b 1
     ) else (
         echo %CHOSEN_DRIVE% drive is NTFS-based.
-        echo Checking %CHOSEN_DRIVE% file system...
-        chkdsk "%CHOSEN_DRIVE%" >nul 2>&1
-        if !errorlevel! neq 0 (
-            echo Repairing %CHOSEN_DRIVE% file system...
-            echo y | chkdsk "%CHOSEN_DRIVE%" /R /X /scan /perf >nul 2>&1
-            echo Restarting system to complete repairs, please save your work.
-            echo Run chkdsk "%CHOSEN_DRIVE%" /sdcleanup after repair finishes.
-            timeout /t 30 /nobreak
-            shutdown /r /t 1 >nul 2>&1
-            exit /b 1
-        )
+        echo Repairing %CHOSEN_DRIVE% file system...
+        echo y | chkdsk "%CHOSEN_DRIVE%" /R /X /scan /perf >nul 2>&1
+        echo Restarting system to complete repairs, please save your work.
+        echo Run chkdsk "%CHOSEN_DRIVE%" /sdcleanup after repair finishes.
+        timeout /t 30 /nobreak
+        shutdown /r /t 1 >nul 2>&1
+        exit /b 1
     )
 )
 
