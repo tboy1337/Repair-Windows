7318db0fd57b244338b683b2e4a36f0416c7e3cb
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-12-07 14:36:13 +0000

    Update chkdsk_Scan.cmd

--- chkdsk_Scan.cmd
+++ chkdsk_Scan.cmd
@@ -9,7 +9,7 @@ if %errorlevel% neq 0 (
     echo This script requires administrator privileges.
     echo Please right-click and select "Run as administrator".
     timeout /t 5 /nobreak
-    exit /b 1
+    exit /b 2
 )
 
 cd /d "%SET_PATH%" >nul 2>&1
@@ -17,11 +17,30 @@ if %errorlevel% neq 0 (
     echo Failed to change to %SET_PATH%
 )
 
-echo Repairing %CHOSEN_DRIVE% file system...
-echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %CHOSEN_DRIVE%^|find "File System Name"') do (
+    echo %%A | findstr /i /r "^FAT" >nul
+    if not errorlevel 1 (
+        echo %CHOSEN_DRIVE% drive is FAT-based.
+        echo Repairing %CHOSEN_DRIVE% file system...
+        echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+        echo Restarting system to complete repairs, please save your work.
+        timeout /t 30 /nobreak
+        shutdown /r /t 1 >nul 2>&1
+        exit /b 0
+    ) else (
+        echo %CHOSEN_DRIVE% drive is NTFS-based.
+        echo Repairing %CHOSEN_DRIVE% file system...
+        echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+        echo Restarting system to complete repairs, please save your work.
+        echo Run chkdsk "%CHOSEN_DRIVE%" /sdcleanup after repair finishes.
+        timeout /t 30 /nobreak
+        shutdown /r /t 1 >nul 2>&1
+        exit /b 0
+    )
+)
 
-echo Restarting system to complete repairs, please save your work.
-timeout /t 30 /nobreak
-shutdown /r /t 1 >nul 2>&1
+echo Found no filesystem on %CHOSEN_DRIVE%
 
-exit /b 0
+timeout /t 5 /nobreak
+
+exit /b 1
