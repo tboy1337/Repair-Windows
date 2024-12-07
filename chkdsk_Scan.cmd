e7e80ae7628fac6c5eaa81ccac496c8cc6ee687d
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-12-07 15:13:28 +0000

    Update chkdsk_Scan.cmd

--- chkdsk_Scan.cmd
+++ chkdsk_Scan.cmd
@@ -17,30 +17,17 @@ if %errorlevel% neq 0 (
     echo Failed to change to %SET_PATH%
 )
 
+echo Repairing %CHOSEN_DRIVE% file system...
+echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+echo Restarting system to complete repairs, please save your work.
+
 for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %CHOSEN_DRIVE%^|find "File System Name"') do (
     echo %%A | findstr /i /r "^FAT" >nul
-    if not errorlevel 1 (
-        echo %CHOSEN_DRIVE% drive is FAT-based.
-        echo Repairing %CHOSEN_DRIVE% file system...
-        echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
-        echo Restarting system to complete repairs, please save your work.
-        timeout /t 30 /nobreak
-        shutdown /r /t 1 >nul 2>&1
-        exit /b 0
-    ) else (
-        echo %CHOSEN_DRIVE% drive is NTFS-based.
-        echo Repairing %CHOSEN_DRIVE% file system...
-        echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
-        echo Restarting system to complete repairs, please save your work.
+    if not errorlevel 0 (
         echo Run chkdsk "%CHOSEN_DRIVE%" /sdcleanup after repair finishes.
-        timeout /t 30 /nobreak
-        shutdown /r /t 1 >nul 2>&1
-        exit /b 0
     )
 )
 
-echo Found no compatible filesystem on %CHOSEN_DRIVE%
-
-timeout /t 5 /nobreak
-
-exit /b 1
+timeout /t 30 /nobreak
+shutdown /r /t 1 >nul 2>&1
+exit /b 0
