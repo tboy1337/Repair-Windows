ece41cf85a7fda8159375016c842f9c4bf8da5e0
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-12-21 17:05:03 +0000

    Update chkdsk_Scan.cmd

--- chkdsk_Scan.cmd
+++ chkdsk_Scan.cmd
@@ -21,19 +21,28 @@ for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %CHOSEN_DRIVE%^|fi
     if not errorlevel 1 (
         echo %CHOSEN_DRIVE% drive is FAT-based.
         echo Repairing %CHOSEN_DRIVE% file system...
-        echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
-        echo Restarting system to complete repairs, please save your work.
-        timeout /t 30 /nobreak
-        shutdown /r /t 1 >nul 2>&1
+        if /i "%CHOSEN_DRIVE%"=="%SystemDrive%" (
+            echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+            echo Restarting system to complete repairs, please save your work.
+            timeout /t 30 /nobreak
+            shutdown /r /t 1 >nul 2>&1
+        ) else (
+            chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+        )
         exit /b 0
     ) else (
         echo %CHOSEN_DRIVE% drive is NTFS-based.
         echo Repairing %CHOSEN_DRIVE% file system...
-        echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
-        echo Restarting system to complete repairs, please save your work.
-        echo Run chkdsk "%CHOSEN_DRIVE%" /sdcleanup after repair finishes.
-        timeout /t 30 /nobreak
-        shutdown /r /t 1 >nul 2>&1
+        if /i "%CHOSEN_DRIVE%"=="%SystemDrive%" (
+            echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+            echo Restarting system to complete repairs, please save your work.
+            echo Run chkdsk "%CHOSEN_DRIVE%" /sdcleanup after repair finishes.
+            timeout /t 30 /nobreak
+            shutdown /r /t 1 >nul 2>&1
+        ) else (
+            chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+            echo Run chkdsk "%CHOSEN_DRIVE%" /sdcleanup after repair finishes.
+        )
         exit /b 0
     )
 )
