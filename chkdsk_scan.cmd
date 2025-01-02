9a44ef46908ca2aecc3b2004042ba3788f8e8d33
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2025-01-02 18:23:34 +0000

    Update chkdsk_scan.cmd

--- chkdsk_scan.cmd
+++ chkdsk_scan.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "CHOSEN_DRIVE=%SystemDrive%"
+set "TARGET_DRIVE=%SystemDrive%"
 
 net session >nul 2>&1
 if %errorlevel% neq 0 (
@@ -16,33 +16,33 @@ if %errorlevel% neq 0 (
     echo Failed to change to %SystemDrive%
 )
 
-for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %CHOSEN_DRIVE%^|find "File System Name"') do (
+for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %TARGET_DRIVE%^|find "File System Name"') do (
     echo %%A | findstr /i /r "^FAT" >nul
     if not errorlevel 1 (
-        echo %CHOSEN_DRIVE% drive is FAT-based.
-        echo Repairing %CHOSEN_DRIVE% file system...
-        if /i "%CHOSEN_DRIVE%"=="%SystemDrive%" (
-            echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+        echo %TARGET_DRIVE% drive is FAT-based.
+        echo Repairing %TARGET_DRIVE% file system...
+        if /i "%TARGET_DRIVE%"=="%SystemDrive%" (
+            echo y | chkdsk "%TARGET_DRIVE%" /R /X >nul 2>&1
             echo Restarting system to complete repairs, please save your work.
             timeout /t 30 /nobreak
             shutdown /r /t 1 >nul 2>&1
         ) else (
-            chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+            chkdsk "%TARGET_DRIVE%" /R /X >nul 2>&1
         )
         exit /b 0
     ) else (
-        echo %CHOSEN_DRIVE% drive is NTFS-based.
-        echo Repairing %CHOSEN_DRIVE% file system...
-        if /i "%CHOSEN_DRIVE%"=="%SystemDrive%" (
-            echo y | chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+        echo %TARGET_DRIVE% drive is NTFS-based.
+        echo Repairing %TARGET_DRIVE% file system...
+        if /i "%TARGET_DRIVE%"=="%SystemDrive%" (
+            echo y | chkdsk "%TARGET_DRIVE%" /R /X >nul 2>&1
             echo Restarting system to complete repairs, please save your work.
-            echo Run chkdsk "%CHOSEN_DRIVE%" /sdcleanup after repair finishes.
+            echo Run chkdsk "%TARGET_DRIVE%" /sdcleanup after repair finishes.
             timeout /t 30 /nobreak
             shutdown /r /t 1 >nul 2>&1
         ) else (
-            chkdsk "%CHOSEN_DRIVE%" /R /X >nul 2>&1
+            chkdsk "%TARGET_DRIVE%" /R /X >nul 2>&1
             echo Cleaning up unnecessary data structures and unallocated metadata files...
-            chkdsk "%CHOSEN_DRIVE%" /sdcleanup >nul 2>&1
+            chkdsk "%TARGET_DRIVE%" /sdcleanup >nul 2>&1
         )
         exit /b 0
     )
