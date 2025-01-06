a239e902b07f11292df045d05e4188478263742e
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2025-01-06 12:24:46 +0000

    Update free_space_quick.cmd

--- free_space_quick.cmd
+++ free_space_quick.cmd
@@ -1,8 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "TARGET_DRIVE=%SystemDrive%"
-
 net session >nul 2>&1
 if %errorlevel% neq 0 (
     echo This script requires administrator privileges.
@@ -16,16 +14,16 @@ if %errorlevel% neq 0 (
     echo Failed to change to %SystemDrive%
 )
 
-echo Freeing up space on %TARGET_DRIVE%...
-call cleanmgr /d "%TARGET_DRIVE%" /verylowdisk >nul 2>&1
+echo Freeing up space on %SystemDrive%...
+call cleanmgr /d "%SystemDrive%" /verylowdisk >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to free up space on %TARGET_DRIVE%
+    echo Failed to free up space on %SystemDrive%
 )
 
-echo Deleting old Windows update files on %TARGET_DRIVE%...
-call cleanmgr /d "%TARGET_DRIVE%" /autoclean >nul 2>&1
+echo Deleting old Windows update files on %SystemDrive%...
+call cleanmgr /d "%SystemDrive%" /autoclean >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to delete old Windows update files on %TARGET_DRIVE%
+    echo Failed to delete old Windows update files on %SystemDrive%
 )
 
 timeout /t 5 /nobreak
