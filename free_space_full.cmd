4f1d43536a72ae00b44ada8ded465dd36e020a13
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2025-01-06 12:27:28 +0000

    Update free_space_full.cmd

--- free_space_full.cmd
+++ free_space_full.cmd
@@ -160,10 +160,10 @@ if %errorlevel% neq 0 (
     echo Failed to set registry key.
 )
 
-echo Freeing up space on %SystemDrive%...
-call cleanmgr /d "%SystemDrive%" /sagerun:9999 >nul 2>&1
+echo Freeing up space on all drives...
+call cleanmgr /sagerun:9999 >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to free up space on %SystemDrive%
+    echo Failed to free up space on all drives.
 )
 
 timeout /t 5 /nobreak
