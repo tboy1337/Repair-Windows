38cf2688fdf9ccfd6f7484e876e1717a4cef39fe
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2025-01-21 01:04:09 +0000

    Update restore_pip_default.cmd

--- restore_pip_default.cmd
+++ restore_pip_default.cmd
@@ -5,21 +5,21 @@ set TEMP_FILE=%TEMP%\installed_packages_%RANDOM%.txt
 echo Restoring pip packages to default...
 
 echo Generating list of installed packages...
-pip freeze > %TEMP_FILE%
+py -m pip freeze > %TEMP_FILE%
 
 echo Uninstalling all packages...
 for /f "delims==" %%p in (%TEMP_FILE%) do (
-    pip uninstall -y %%p
+    py -m pip uninstall -y %%p
 )
 
 echo Reinstalling default packages...
 py -m ensurepip --upgrade
-pip install --upgrade setuptools wheel
+py -m pip install --upgrade setuptools wheel
 
 del %TEMP_FILE%
 
 echo Purging pip cache...
-pip cache purge
+py -m pip cache purge
 
 echo Done! Your pip packages have been restored to default.
 
