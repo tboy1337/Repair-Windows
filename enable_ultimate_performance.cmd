9d41d448d96f73568f2417ea383b1d76f93c1961
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2025-02-03 14:21:41 +0100

    Update enable_ultimate_performance.cmd

--- enable_ultimate_performance.cmd
+++ enable_ultimate_performance.cmd
@@ -14,7 +14,11 @@ if %errorlevel% neq 0 (
     echo Failed to change to %SystemDrive%
 )
 
+echo Enabling ultimate power plan...
 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+if %errorlevel% neq 0 (
+    echo Failed to enable ultimate power plan.
+)
 
 timeout /t 5 /nobreak
-exit
+exit /b 0
