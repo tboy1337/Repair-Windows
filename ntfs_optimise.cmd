0fba5ed0a003eaeed5eb2131a64c2515498d306f
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2025-02-03 14:33:30 +0100

    Create ntfs_optimise.cmd

---
+++ ntfs_optimise.cmd
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo This script requires administrator privileges.
+    echo Please right-click and select "Run as administrator".
+    timeout /t 5 /nobreak
+    exit /b 1
+)
+
+cd /d "%SystemDrive%" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Failed to change to %SystemDrive%
+)
+
+echo Increasing the memory usage for NTFS metadata...
+fsutil behavior set memoryusage 2
+if %errorlevel% neq 0 (
+    echo Failed to increase the memory usage for NTFS metadata.
+)
+
+echo Reserving more space for the Master File Table (MFT)...
+fsutil behavior set mftzone 2
+if %errorlevel% neq 0 (
+    echo Failed to reserve more space for the Master File Table (MFT).
+)
+
+timeout /t 5 /nobreak
+exit /b 0
