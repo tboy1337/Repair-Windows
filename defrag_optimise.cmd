a694339542087aeeff8b27188973cb7091a09311
tboy1337 <tboy1337.unchanged733@aleeas.com>
2025-07-27 12:53:34 +0100

    Enhance error reporting in defrag_optimise.cmd by including error codes in failure messages for better debugging and clarity.

--- defrag_optimise.cmd
+++ defrag_optimise.cmd
@@ -11,43 +11,43 @@ if %errorlevel% neq 0 (
 
 cd /d "%SystemDrive%" >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to change to %SystemDrive%
+    echo Failed to change to %SystemDrive%.  Error code: %errorlevel%
 )
 
 echo Analysing all drives...
 defrag /C /A /H >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to analyze all drives.
+    echo Failed to analyze all drives.  Error code: %errorlevel%
 )
 
 echo Performing boot optimization on %SystemDrive%...
 defrag "%SystemDrive%" /B /H >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to perform boot optimization on %SystemDrive%
+    echo Failed to perform boot optimization on %SystemDrive%.  Error code: %errorlevel%
 )
 
 echo Optimizing the storage tiers on all drives...
 defrag /C /G /I 300 /H >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to optimize the storage tiers on all drives.
+    echo Failed to optimize the storage tiers on all drives.  Error code: %errorlevel%
 )
 
 echo Performing slab consolidation on all drives...
 defrag /C /K /H >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to perform slab consolidation on all drives.
+    echo Failed to perform slab consolidation on all drives.  Error code: %errorlevel%
 )
 
 echo Optimizing all drives (might take a long time)...
 defrag /C /O /H >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to optimize all drives.
+    echo Failed to optimize all drives.  Error code: %errorlevel%
 )
 
 echo Performing free space consolidation on all drives...
 defrag /C /X /H >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Failed to perform free space consolidation on all drives.
+    echo Failed to perform free space consolidation on all drives.  Error code: %errorlevel%
 )
 
 timeout /t 5 /nobreak
