1a9d32d4762f29bba02ab290833b15d365941552
tboy1337 <tboy1337.unchanged733@aleeas.com>
2025-07-30 00:21:44 +0100

    Add timeout prompt in enable_ultimate_performance.cmd for improved error handling

--- enable_ultimate_performance.cmd
+++ enable_ultimate_performance.cmd
@@ -18,6 +18,7 @@ echo Enabling ultimate power plan...
 powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61 >nul 2>&1
 if %errorlevel% neq 0 (
     echo Failed to enable ultimate power plan.  Error code: %errorlevel%
+    timeout /t 5 /nobreak
     exit /b 1
 )
 
