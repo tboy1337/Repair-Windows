e0235ca0b19e5e046eb36f7a53694f8038c8ad18
tboy1337 <tboy1337.unchanged733@aleeas.com>
2025-08-01 12:51:47 +0100

    Enhance restore_pip_default.cmd by redirecting output to null for pip commands, improving script cleanliness and reducing verbosity. Added error handling for pip upgrade and cache purge operations to provide clearer feedback on failures.

--- Misc/restore_pip_default.cmd
+++ Misc/restore_pip_default.cmd
@@ -6,21 +6,31 @@ set TEMP_FILE=%TEMP%\installed_packages_%RANDOM%.txt
 echo Restoring pip packages to default...
 
 echo Generating list of installed packages...
-py -m pip freeze > %TEMP_FILE%
+py -m pip freeze > %TEMP_FILE% >nul
 
 echo Uninstalling all packages...
 for /f "delims==" %%p in (%TEMP_FILE%) do (
-    py -m pip uninstall -y %%p
+    py -m pip uninstall -y %%p >nul 2>&1
 )
 
 echo Reinstalling default packages...
-py -m ensurepip --upgrade
-py -m pip install --upgrade setuptools wheel
+py -m ensurepip --upgrade >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Failed to upgrade pip.  Error code: %errorlevel%
+)
+
+py -m pip install --upgrade setuptools wheel >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Failed to upgrade setuptools and wheel.  Error code: %errorlevel%
+)
 
 del %TEMP_FILE%
 
 echo Purging pip cache...
-py -m pip cache purge
+py -m pip cache purge >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Failed to purge pip cache.  Error code: %errorlevel%
+)
 
 timeout /t 5 /nobreak
 exit
