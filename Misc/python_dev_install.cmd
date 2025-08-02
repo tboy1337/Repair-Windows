90432ff8791cd910979f6a71b2a3ab2962f9f1b8
tboy1337 <tboy1337.unchanged733@aleeas.com>
2025-08-02 09:56:43 +0100

    Refine error message in python_dev_install.cmd by removing "ERROR:" prefix for a more straightforward user experience when Python is not found in PATH.

--- Misc/python_dev_install.cmd
+++ Misc/python_dev_install.cmd
@@ -18,7 +18,7 @@ if %errorlevel% equ 0 (
     if %errorlevel% equ 0 (
         set PYTHON_CMD=python
     ) else (
-        echo ERROR: Python is not installed or not in PATH.
+        echo Python is not installed or not in PATH.
         timeout /t 5 /nobreak
         exit /b 1
     )
