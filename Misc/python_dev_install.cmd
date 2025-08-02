87de740f837113966de32401d9bf3b52134bb85c
tboy1337 <tboy1337.unchanged733@aleeas.com>
2025-08-02 14:20:55 +0100

    Replace flake8 installation with black and isort in python_dev_install.cmd to update code formatting tools for improved development workflow.

--- Misc/python_dev_install.cmd
+++ Misc/python_dev_install.cmd
@@ -38,7 +38,8 @@ echo Starting Package Installation/Upgrade...
 %PYTHON_CMD% -m pip install --upgrade virtualenv
 %PYTHON_CMD% -m pip install --upgrade python-dotenv
 %PYTHON_CMD% -m pip install --upgrade pylint
-%PYTHON_CMD% -m pip install --upgrade flake8
+%PYTHON_CMD% -m pip install --upgrade black
+%PYTHON_CMD% -m pip install --upgrade isort
 %PYTHON_CMD% -m pip install --upgrade pytest
 %PYTHON_CMD% -m pip install --upgrade pytest-asyncio
 %PYTHON_CMD% -m pip install --upgrade pytest-timeout
