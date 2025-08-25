908591ab12c9bfd2c4124de0dae0455632d4b3ec
tboy1337 <tboy1337.unchanged733@aleeas.com>
2025-08-25 18:09:26 +0100

    Refactor defrag_optimise.cmd to always perform free space consolidation, removing SSD check for improved consistency in drive optimization.

--- defrag_optimise.cmd
+++ defrag_optimise.cmd
@@ -61,14 +61,10 @@ if %errorlevel% neq 0 (
     echo Failed to optimize all drives.  Error code: %errorlevel%
 )
 
-if "!IsSSD!"=="true" (
-    echo %SystemDrive% is an SSD - skipping free space consolidation...
-) else (
-    echo Performing free space consolidation on all drives...
-    defrag /C /X /H >nul 2>&1
-    if %errorlevel% neq 0 (
-        echo Failed to perform free space consolidation on all drives.  Error code: %errorlevel%
-    )
+echo Performing free space consolidation on all drives...
+defrag /C /X /H >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Failed to perform free space consolidation on all drives.  Error code: %errorlevel%
 )
 
 timeout /t 5 /nobreak
