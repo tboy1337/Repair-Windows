6d874e95f73cd98392c6474058170e633146178b
tboy1337 <check@my-website.com>
2025-10-07 11:59:45 +0100

    Update repair_drive_mid.cmd to enhance user messaging by adding parentheses around "might take a long time" for improved clarity during the repair process. This change ensures consistency in notifications across different drive types.

--- repair_drive_mid.cmd
+++ repair_drive_mid.cmd
@@ -20,7 +20,7 @@ for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %TARGET_DRIVE%^|fi
     echo %%A | findstr /i /r "^FAT" >nul
     if !errorlevel! equ 0 (
         echo %TARGET_DRIVE% drive is FAT-based.
-        echo Repairing %TARGET_DRIVE% file system (might take a long time)...
+        echo Repairing %TARGET_DRIVE% file system ^(might take a long time^)...
         if /i "%TARGET_DRIVE%"=="%SystemDrive%" (
             echo y | chkdsk "%TARGET_DRIVE%" /R /X >nul 2>&1
             echo Restarting system to complete repairs, please save your work!
@@ -32,7 +32,7 @@ for /f "tokens=4 delims=: " %%A in ('fsutil fsinfo volumeinfo %TARGET_DRIVE%^|fi
         )
     ) else (
         echo %TARGET_DRIVE% drive is NTFS-based.
-        echo Repairing %TARGET_DRIVE% file system (might take a long time)...
+        echo Repairing %TARGET_DRIVE% file system ^(might take a long time^)...
         if /i "%TARGET_DRIVE%"=="%SystemDrive%" (
             echo y | chkdsk "%TARGET_DRIVE%" /R /X >nul 2>&1
             echo Restarting system to complete repairs, please save your work!
